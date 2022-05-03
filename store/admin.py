182759d336383a4601c530d37fbcf8a5dd6180b8
Rakhmatullo929 <tillo3305@gmail.com>
2022-05-03 16:31:39 +0300

    master changes

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
 # Register your models here.
+from .models import *
+
+
+class SlugAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)},
--- womazing/urls.py
+++ womazing/urls.py
@@ -22,6 +22,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(('store.urls', 'womazing'), namespace='store')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
